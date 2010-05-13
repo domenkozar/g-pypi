03d3b649734036c1b310ac10123f86bfd5533081
Domen Kožar <domen@dev.si>
2010-05-14 00:24:16 +0200

    adding portage setup.py file, for easier unittesting

---
+++ portage_setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+This file is here to be copied to portage root
+and used for distribution in for easier unittesting
+"""
+
+from setuptools import setup
+
+version='0.1'
+
+setup(name="portage",
+    license="GPL-2",
+    version=version,
+    description="",
+    long_description="",
+    maintainer="",
+    author="",
+    author_email="",
+    url="",
+    keywords="",
+    classifiers=[],
+    packages=['portage'],
+    package_dir={'portage':'pym/portage'},
+    include_package_data = True,
+)
